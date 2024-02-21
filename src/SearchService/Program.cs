066e61547207a183eec0a1629c81c628cf99c85d
George Skouras <g.skouras@kaizengaming.com>
2024-02-21 16:21:10 +0200

    feat: add Search Service and sync with AuctionService

---
+++ src/SearchService/Program.cs
@@ -0,0 +1,38 @@
+using System.Net;
+// using MassTransit;
+using Polly;
+using Polly.Extensions.Http;
+using SearchService;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddHttpClient<AuctionSvcHttpClient>().AddPolicyHandler(GetPolicy());
+
+builder.Services.AddEndpointsApiExplorer();
+
+var app = builder.Build();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+try
+{
+    await DbInitializer.InitDb(app);
+}
+catch (Exception e)
+{
+    System.Console.WriteLine(e);
+}
+
+app.Run();
+
+static IAsyncPolicy<HttpResponseMessage> GetPolicy()
+    => HttpPolicyExtensions
+        .HandleTransientHttpError()
+        .OrResult(msg => msg.StatusCode == HttpStatusCode.NotFound)
+        .WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(3));
